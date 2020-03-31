b8c158b02722823888e33f018d4905f52b392602
Elizabeth <elizabeth.brooks@yale.edu>
2020-03-30 23:39:34 -0400

    Added "add collaborator" to CreateProjectPage

--- src/CreateProjectPage/CreateProjectPage.jsx
+++ src/CreateProjectPage/CreateProjectPage.jsx
@@ -10,7 +10,8 @@ class CreateProjectPage extends React.Component {
         this.state = {
             projectTitle: '',
             projectDescription: '',
-            collaborators = [],
+            currCollab: '',
+            collaborators: ["he"],
             submitted: false,
             loading: false,
             error: ''
@@ -18,11 +19,18 @@ class CreateProjectPage extends React.Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCollabChange = this.handleCollabChange.bind(this);
     }
 
     handleChange(e) {
         const { name, value } = e.target;
         this.setState({ [name]: value });
+    }
+
+    handleCollabChange() {
+        const joined = this.state.collaborators.concat(this.state.currCollab);
+        this.setState({ collaborators: joined });
+        this.setState({currCollab: ''});
     }
 
     handleSubmit(e) {
@@ -47,9 +55,19 @@ class CreateProjectPage extends React.Component {
             );
     }
 
+    displayCollaborators(){
+        const collaborators = this.state.collaborators;
+        const listItems = collaborators.map((collaborator) =>
+            <li>{collaborator}</li>
+        );
+
+        return (
+            <ul>{listItems}</ul>
+        );
+    }
     
     render() {
-        const { projectTitle, projectDescription, collaborators, submitted, loading, error } = this.state;
+        const { projectTitle, projectDescription, currCollab, collaborators, submitted, loading, error } = this.state;
         return (
             <div>
                 <div className="row form-row">
@@ -74,7 +92,7 @@ class CreateProjectPage extends React.Component {
                     <div className={'form-group form-row' + (submitted && !projectDescription ? ' has-error' : '')}>
                         <label className="col-sm-4 horLabel" htmlFor="projectDescription">Project Description</label>
                         <div className="col-sm-8">
-                        <textarea type="textarea" className="form-control textarea" rows="5" name="projectDescription" value={projectDescription} onChange={this.handleChange} />
+                            <textarea type="textarea" className="form-control textarea" rows="5" name="projectDescription" value={projectDescription} onChange={this.handleChange} />
                             {submitted && !projectDescription &&
                                 <div className="help-block">Project description is required</div>
                             }
@@ -86,10 +104,24 @@ class CreateProjectPage extends React.Component {
                         <h3>Add Collaborators</h3>
                     </div>
 
+                    <div className={'form-group form-row'}>
+                        <div className="col-sm-3"></div>
+                        <div className="col-sm-4">
+                            <input type="text" className="form-control" name="currCollab" value={currCollab} onChange={this.handleChange} placeholder="Enter collaborator's email"/>
+                        </div>
+                        <div className="col-sm-2">
+                            <button type="button" className="btn btn-primary" onClick={this.handleCollabChange}>Add</button>
+                        </div>
+                        <div className="col-sm-3"></div>
+                    </div>
+                    
+                    <div>
+                        {this.displayCollaborators()}
+                    </div>
                     
                    
-                    <div className="form-group">
-                        <button className="btn btn-primary" disabled={loading}>Login</button>
+                    <div className="form-group submit">
+                        <button className="btn btn-primary" disabled={loading} >Login</button>
                         {loading &&
                             <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
                         }
